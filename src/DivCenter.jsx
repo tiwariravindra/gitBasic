f68395a4af87d26696e8a15a718f5821387a95a3
Ravindra Tiwari <tiwariravindra91@gmail.com>
2025-01-19 01:15:51 +0530

    Updated DivCenter file

--- src/DivCenter.jsx
+++ src/DivCenter.jsx
@@ -4,6 +4,7 @@ import React from "react";
 const DivCenter = () => {
     return (<div className="upperCenter">
             <div className="center">Center</div>
+            <div className="center">Add another Div to be in Center</div>
         </div>);
 };
 
